0b786687d35738063920fb30536bc864f07afe5c
eebrown <eb@ericebrown.com>
2020-02-01 14:36:58 -0500

    compiled vignette

---
+++ doc/WHO_SR.R
@@ -0,0 +1,26 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+library(data2019nCoV)
+
+## -----------------------------------------------------------------------------
+plot(WHO_SR$Date, WHO_SR$Global.confirmed, main = "Global 2019-CoV Confirmed Cases",
+                                           xlab = "Date",
+                                           ylab = "Confirmed Cases")
+
+## -----------------------------------------------------------------------------
+plot(WHO_SR$Date, WHO_SR$China.deaths,     main = "2019-CoV Deaths (Logarithmic)",
+                                           xlab = "Date",
+                                           ylab = "Deaths",
+                                           log = "y")
+
+plot(WHO_SR$Date, WHO_SR$Global.confirmed, main = "Global 2019-CoV Confirmed Cases (Logarithmic)",
+                                           xlab = "Date",
+                                           ylab = "Confirmed Cases",
+                                           log = "y")
+
+
