576e8c7a8594cbd103a6b374594b2f233f4abd2d
eebrown <eb@ericebrown.com>
2020-02-01 17:54:27 -0500

    updated with Chinese province-level data from SRs; documentation changes.

--- README.md
+++ README.md
@@ -4,12 +4,14 @@ This is a simple R package to store data about the 2019-2020 novel coronavirus o
 
 The package can be installed via `devtools::install_github("eebrown/data2019nCoV")`.
 
+More information can be found in the [vignette](https://htmlpreview.github.io/?https://github.com/eebrown/data2019nCoV/blob/master/doc/WHO_SR.html). 
+
 ## Contributions
 
-Contributions are welcome in the form of data updates, corrections, and the contribution of new datasets, provided they are publicly available, verifiable, and from a reputable source.
+Contributions are welcome in the form of updates, corrections, and the contribution of new datasets, provided they are publicly available, verifiable, and from a reputable source.
 
 Raw data files (e.g. `.csv`) are in the `/raw-data` directory, along with a `.R` file that can be run to generate the data object for the package.
 
 ## Licence
 
-This package uses freely available data, and itself is freely available with the CC0 licence.
+This package uses freely available data, and itself is freely available with the CC0 licence. Information from WHO may be subject to WHO [copyright and terms](https://www.who.int/publishing/copyright/en/). The data source and package should be attributed. No warranty is made as to the accuracy of the data: use at your own risk. Please file and issue if you find any errors. 
--- doc/WHO_SR.R
+++ doc/WHO_SR.R
@@ -7,20 +7,23 @@ knitr::opts_chunk$set(
 ## ----setup--------------------------------------------------------------------
 library(data2019nCoV)
 
-## -----------------------------------------------------------------------------
-plot(WHO_SR$Date, WHO_SR$Global.confirmed, main = "Global 2019-CoV Confirmed Cases",
-                                           xlab = "Date",
-                                           ylab = "Confirmed Cases")
+## ---- fig.width=6-------------------------------------------------------------
+plot(WHO_SR$Date, WHO_SR$Global.confirmed, 
+     main = "Global 2019-CoV Confirmed Cases",
+     xlab = "Date",
+     ylab = "Confirmed Cases")
 
-## -----------------------------------------------------------------------------
-plot(WHO_SR$Date, WHO_SR$China.deaths,     main = "2019-CoV Deaths (Logarithmic)",
-                                           xlab = "Date",
-                                           ylab = "Deaths",
-                                           log = "y")
+## ---- fig.width=6-------------------------------------------------------------
+plot(WHO_SR$Date, WHO_SR$China.deaths,
+     main = "2019-CoV Deaths (Logarithmic)",
+     xlab = "Date",
+     ylab = "Deaths",
+     log = "y")
 
-plot(WHO_SR$Date, WHO_SR$Global.confirmed, main = "Global 2019-CoV Confirmed Cases (Logarithmic)",
-                                           xlab = "Date",
-                                           ylab = "Confirmed Cases",
-                                           log = "y")
+plot(WHO_SR$Date, WHO_SR$Global.confirmed, 
+     main = "Global 2019-CoV Confirmed Cases (Logarithmic)",
+     xlab = "Date",
+     ylab = "Confirmed Cases",
+     log = "y")
 
 
