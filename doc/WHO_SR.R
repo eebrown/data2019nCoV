044f853b4f138928033c151a457572ef7551c836
eebrown <eb@ericebrown.com>
2020-02-01 19:12:25 -0500

    added ggplot example to vignette

--- doc/WHO_SR.R
+++ doc/WHO_SR.R
@@ -7,13 +7,26 @@ knitr::opts_chunk$set(
 ## ----setup--------------------------------------------------------------------
 library(data2019nCoV)
 
-## ---- fig.width=6-------------------------------------------------------------
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+
+## ---- fig.width=6, fig.height=6-----------------------------------------------
 plot(WHO_SR$Date, WHO_SR$Global.confirmed, 
      main = "Global 2019-CoV Confirmed Cases",
      xlab = "Date",
      ylab = "Confirmed Cases")
 
-## ---- fig.width=6-------------------------------------------------------------
+gather(WHO_SR, key, value, Japan, RepublicofKorea, VietNam, Singapore, Australia, Malaysia, Cambodia, Philippines, Thailand, Nepal, SriLanka, India, UnitedStatesofAmerica, Canada, Finland, France, Germany, Italy, UnitedArabEmirates) %>%
+  ggplot(aes(x=Date, y=value, colour=key)) +
+  geom_line() +
+  theme(legend.position="bottom") +
+  labs(title = "Confirmed Cases Outside of China",
+       x = "Date", 
+       y = "Confirmed Cases") +
+  theme(legend.title = element_blank())
+
+## ---- fig.width=6, fig.height=6-----------------------------------------------
 plot(WHO_SR$Date, WHO_SR$China.deaths,
      main = "2019-CoV Deaths (Logarithmic)",
      xlab = "Date",
