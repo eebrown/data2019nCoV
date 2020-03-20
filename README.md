0ec4aa0f7a4b801cf707ff9233567be0ffcda515
eebrown <eb@ericebrown.com>
2020-03-20 11:17:30 -0400

    2020-03-20 Ontario morning results

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Currently, this package contains 3 datasets.
 
 ### 2. Ontario Ministry of Health Cumulative Cases
 
-`ON_cumulative`: The source of this data is the official Ontario Government's [website](https://www.ontario.ca/page/2019-novel-coronavirus). Data is included from page updates that were captured by the [Internet Archive](https://web.archive.org/web/*/https://www.ontario.ca/page/2019-novel-coronavirus) where possible. It has been updated up to 2020-03-19.
+`ON_cumulative`: The source of this data is the official Ontario Government's [website](https://www.ontario.ca/page/2019-novel-coronavirus). Data is included from page updates that were captured by the [Internet Archive](https://web.archive.org/web/*/https://www.ontario.ca/page/2019-novel-coronavirus) where possible. It has been updated up to 2020-03-20.
 
 ### 3. Government of Canada Cumulative Cases
 
--- doc/ON_cumulative.R
+++ doc/ON_cumulative.R
@@ -7,6 +7,13 @@ knitr::opts_chunk$set(
 ## ----setup--------------------------------------------------------------------
 library(data2019nCoV)
 
+daily_change <- function(series) {
+  change <- c(series, NA) - c(NA, series)
+  change <- change[-1]
+  change <- change[-length(change)]
+  return(change)
+}
+
 ## ---- fig.width=6, fig.height=10----------------------------------------------
 
 par(mfrow=c(2,1))
@@ -18,5 +25,13 @@ plot(ON_cumulative$LastUpdated, (ON_cumulative$ConfirmedPositive+ON_cumulative$R
      ylab = "Current and Resolved Cases",
      type = "b")
 
+plot(ON_cumulative$LastUpdated[-1], (daily_change(ON_cumulative$Negative + 
+                                 ON_cumulative$ConfirmedPositive + 
+                                 ON_cumulative$Resolved +
+                                 ON_cumulative$Deceased)),
+     main = "Number of Results Posted Between Reports in Ontario",
+     xlab = "Date",
+     ylab = "Change in Negative + Positive + Resolved + Deceased",
+     type = "b")
 
 
